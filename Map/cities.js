ea4c79c04f61e73d3ff10cf6b4b52b4b71d6cbab
april Prospero Garciagit config --global user.email april.prospero@gmail.com <april.prospero@gmail.com>
2021-03-08 07:20:01 -0700

    finished map

---
+++ Map/cities.js
@@ -0,0 +1,252 @@
+var cities = [
+ {
+   "State": "Alabama",
+   "Longitude": "-86.902298",
+   "Latitude": "32.31823"
+ },
+ {
+   "State": "Alaska",
+   "Longitude": "-153.369141",
+   "Latitude": "66.160507"
+ },
+ {
+   "State": "Arizona",
+   "Longitude": "-111.093735",
+   "Latitude": "34.048927"
+ },
+ {
+   "State": "Arkansas",
+   "Longitude": "-92.199997",
+   "Latitude": "34.799999"
+ },
+ {
+   "State": "California",
+   "Longitude": "-119.417931",
+   "Latitude": "36.778259"
+ },
+ {
+   "State": "Colorado",
+   "Longitude": "-105.358887",
+   "Latitude": "39.113014"
+ },
+ {
+   "State": "Connecticut",
+   "Longitude": "-72.699997",
+   "Latitude": "41.599998"
+ },
+ {
+   "State": "Delaware",
+   "Longitude": "-75.5",
+   "Latitude": "39"
+ },
+ {
+   "State": "Florida",
+   "Longitude": "-81.760254",
+   "Latitude": "27.994402"
+ },
+ {
+   "State": "Georgia",
+   "Longitude": "-83.441162",
+   "Latitude": "33.247875"
+ },
+ {
+   "State": "Hawaii",
+   "Longitude": "-155.844437",
+   "Latitude": "19.741755"
+ },
+ {
+   "State": "Idaho",
+   "Longitude": "-114.742043",
+   "Latitude": "44.068203"
+ },
+ {
+   "State": "Illinois",
+   "Longitude": "-89",
+   "Latitude": "40"
+ },
+ {
+   "State": "Indiana",
+   "Longitude": "-86.126976",
+   "Latitude": "40.273502"
+ },
+ {
+   "State": "Iowa",
+   "Longitude": "-93.581543",
+   "Latitude": "42.032974"
+ },
+ {
+   "State": "Kansas",
+   "Longitude": "-98",
+   "Latitude": "38.5"
+ },
+ {
+   "State": "Kentucky",
+   "Longitude": "-84.27002",
+   "Latitude": "37.839333"
+ },
+ {
+   "State": "Louisiana",
+   "Longitude": "-92.329102",
+   "Latitude": "30.39183"
+ },
+ {
+   "State": "Maine",
+   "Longitude": "-68.972168",
+   "Latitude": "45.367584"
+ },
+ {
+   "State": "Maryland",
+   "Longitude": "-76.641273",
+   "Latitude": "39.045753"
+ },
+ {
+   "State": "Massachusetts",
+   "Longitude": "-71.382439",
+   "Latitude": "42.407211"
+ },
+ {
+   "State": "Michigan",
+   "Longitude": "-84.506836",
+   "Latitude": "44.182205"
+ },
+ {
+   "State": "Minnesota",
+   "Longitude": "-94.63623",
+   "Latitude": "46.39241"
+ },
+ {
+   "State": "Mississippi",
+   "Longitude": "-90",
+   "Latitude": "33"
+ },
+ {
+   "State": "Missouri",
+   "Longitude": "-92.60376",
+   "Latitude": "38.573936"
+ },
+ {
+   "State": "Montana",
+   "Longitude": "-109.533691",
+   "Latitude": "46.96526"
+ },
+ {
+   "State": "Nebraska",
+   "Longitude": "-100",
+   "Latitude": "41.5"
+ },
+ {
+   "State": "Nevada",
+   "Longitude": "-117.224121",
+   "Latitude": "39.876019"
+ },
+ {
+   "State": "New Hampshire",
+   "Longitude": "-71.5",
+   "Latitude": "44"
+ },
+ {
+   "State": "New Jersey",
+   "Longitude": "-74.871826",
+   "Latitude": "39.833851"
+ },
+ {
+   "State": "New Mexico",
+   "Longitude": "-106.018066",
+   "Latitude": "34.307144"
+ },
+ {
+   "State": "New York",
+   "Longitude": "-75",
+   "Latitude": "43"
+ },
+ {
+   "State": "North Carolina",
+   "Longitude": "-80.793457",
+   "Latitude": "35.782169"
+ },
+ {
+   "State": "North Dakota",
+   "Longitude": "-100.437012",
+   "Latitude": "47.650589"
+ },
+ {
+   "State": "Ohio",
+   "Longitude": "-82.996216",
+   "Latitude": "40.367474"
+ },
+ {
+   "State": "Oklahoma",
+   "Longitude": "-96.921387",
+   "Latitude": "36.084621"
+ },
+ {
+   "State": "Oregon",
+   "Longitude": "-120.5",
+   "Latitude": "44"
+ },
+ {
+   "State": "Pennsylvania",
+   "Longitude": "-77.194527",
+   "Latitude": "41.203323"
+ },
+ {
+   "State": "Rhode Island",
+   "Longitude": "-71.5",
+   "Latitude": "41.700001"
+ },
+ {
+   "State": "South Carolina",
+   "Longitude": "-81.163727",
+   "Latitude": "33.836082"
+ },
+ {
+   "State": "South Dakota",
+   "Longitude": "-100",
+   "Latitude": "44.5"
+ },
+ {
+   "State": "Tennessee",
+   "Longitude": "-86.660156",
+   "Latitude": "35.860119"
+ },
+ {
+   "State": "Texas",
+   "Longitude": "-100",
+   "Latitude": "31"
+ },
+ {
+   "State": "Utah",
+   "Longitude": "-111.950684",
+   "Latitude": "39.41922"
+ },
+ {
+   "State": "Vermont",
+   "Longitude": "-72.699997",
+   "Latitude": "44"
+ },
+ {
+   "State": "Virginia",
+   "Longitude": "-78.024902",
+   "Latitude": "37.926868"
+ },
+ {
+   "State": "Washington",
+   "Longitude": "#N/A",
+   "Latitude": "#N/A"
+ },
+ {
+   "State": "West Virginia",
+   "Longitude": "-80.5",
+   "Latitude": "39"
+ },
+ {
+   "State": "Wisconsin",
+   "Longitude": "-89.5",
+   "Latitude": "44.5"
+ },
+ {
+   "State": "Wyoming",
+   "Longitude": "-107.290283",
+   "Latitude": "43.07597"
+ }
+]
